d518a8e09622d269a2ac0df3707c6ca6540ce112
jabayaki <jabayaki-system>
2024-12-11 14:09:05 +0900

    userの権限の追加

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -9,24 +9,33 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
     echo "Configuring MariaDB..."
-    cat <<EOF > /tmp/init.sql
+    cat <<EOF > /tmp/init.txt
 USE mysql;
 FLUSH PRIVILEGES;
 
+-- rootユーザーの設定
+CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_PASSWORD}';
+-- ユーザーの設定
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost';
 
+-- 匿名ユーザーの削除
+DELETE FROM mysql.user WHERE user = '';
 FLUSH PRIVILEGES;
+
 EOF
 
     # bootstrapで初期設定
-    mysqld --user=mysql --bootstrap < /tmp/init.sql
+    mysqld --user=mysql --bootstrap < /tmp/init.txt
 
-    rm -f /tmp/init.sql
+    rm -f /tmp/init.txt
 
     echo "MariaDB configuration completed." 
 else
