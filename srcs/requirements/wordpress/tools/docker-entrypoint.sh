18da0cc397fe4d68105063e4498f7d48c55bfdcd
jabayaki <jabayaki-system>
2024-12-13 00:24:46 +0900

    mariadbとの紐付けに伴いenvの名前で登録

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 echo "Starting WordPress setup..."
 
 # 必要な環境変数を確認
-if [ -z "$WORDPRESS_DB_HOST" ] || [ -z "$WORDPRESS_DB_USER" ] || [ -z "$WORDPRESS_DB_PASSWORD" ] || [ -z "$WORDPRESS_DB_NAME" ]; then
+if [ -z "$MYSQL_HOST" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ]; then
     echo "Error: Missing required environment variables." >&2
     exit 1
 fi
@@ -20,10 +20,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 
     echo "Configuring WordPress..."
     cp wp-config-sample.php wp-config.php
-    sed -i "s/database_name_here/$WORDPRESS_DB_NAME/" wp-config.php
-    sed -i "s/username_here/$WORDPRESS_DB_USER/" wp-config.php
-    sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" wp-config.php
-    sed -i "s/localhost/$WORDPRESS_DB_HOST/" wp-config.php
+    sed -i "s/database_name_here/$MYSQL_NAME/" wp-config.php
+    sed -i "s/username_here/$MYSQL_USER/" wp-config.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/" wp-config.php
+    sed -i "s/localhost/$MYSQL_HOST/" wp-config.php
 fi
 
 echo "WordPress setup completed."
